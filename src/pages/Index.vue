c854cc04d91cd653583b9a9ba7646ba1ff1107e9
Cute-Fox <74364811+Cute-Fox@users.noreply.github.com>
2020-11-14 01:36:57 +0300

    Левое меню текста и диалог 1:36

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,14 +8,43 @@
             <q-avatar rounded size="65px" font-size="42px" color="red" icon="F" text-color="white"/>
           </div>
           <div class="text-subtitle2 text-weight-bold margine">{{name}} <br> {{sorname}} </div>
+          <div class="text-h6 text-weight-bold margine2"> {{balance}} баллов</div>
         </div>
-        <div class="row">
+        <div class="row margine">
           <div class="col">
-            <div class="full-width text-subtitle2 text-weight-bold text-center">
+            <div class="full-width text-body1 text-center">
               Ваша доставка
             </div>
             <div class="q-pa-md">
-              <q-linear-progress rounded size="15px" :value="progress" color="warning" class="q-mt-20" />
+              <q-linear-progress rounded size="10px" :value="progress" color="purple" class="q-mt-20" />
+            </div>
+            <div class="full-width text-body1 margine">
+              Осталось: {{timeM}} мин <br> Состояние: {{Stage}}
+            </div>
+            <div class="full-width text-body1 margine2">
+              Отправитель: {{sender}}
+              <q-btn flat color="primary" label="Подробнее" @click="bottomClick1" />
+              <q-dialog v-model="icon">
+                <q-card>
+                  <q-card-section class="row items-center q-pb-none">
+                    <div class="text-h4">Об отправителе</div>
+                    <q-space />
+                    <q-btn icon="close" flat round dense v-close-popup />
+                  </q-card-section>
+
+                  <q-card-section class="text-subtitle2">
+                    Отправитель: {{sender}} <br>
+                    Адресс: {{senderAdres}} <br>
+                    Отправлено в {{timeSend}}
+                  </q-card-section>
+                  <q-card-section class="text-body3">
+                    {{senderGroupPost}}
+                  </q-card-section>
+                </q-card>
+              </q-dialog>
+              <div class="full-width text-body1 margine2">
+                Получатель: {{name}} {{sorname}}
+              </div>
             </div>
           </div>
         </div>
@@ -55,7 +84,20 @@ export default {
       progress: 0.84,
       left: false,
       name: 'Ник',
-      sorname: 'Уайлд'
+      sorname: 'Уайлд',
+      balance: 300,
+      timeM: 18,
+      Stage: 'Доставка',
+      sender: 'McDonalds',
+      icon: false,
+      senderAdres: 'OOO фиг знает где. Может на камчатке',
+      timeSend: '16:44',
+      senderGroupPost: 'ООО McDonalds пожри и будь жирным'
+    }
+  },
+  methods: {
+    bottomClick1 () {
+      this.icon = true
     }
   }
 }
@@ -72,6 +114,9 @@ export default {
     size: 150px;
   }
   .margine {
-    margin: 21px;
+    margin: 13px;
+  }
+  .margine2 {
+    margin: 28px;
   }
 </style>
